8e47bbe3eb3efb80e3f7ecbbff8e8feeab8b7b99
David Genord II <david@collectiveidea.com>
2025-03-25 10:01:17 -0400

    Don't fallback to shell out content type detection
    This is to limit interaction with the file before running security scan on S3

--- lib/paperclip/content_type_detector.rb
+++ lib/paperclip/content_type_detector.rb
@@ -60,7 +60,7 @@ module Paperclip
     end
 
     def type_from_file_contents
-      type_from_marcel || type_from_file_command
+      type_from_marcel
     rescue Errno::ENOENT => e
       Paperclip.log("Error while determining content type: #{e}")
       SENSIBLE_DEFAULT
